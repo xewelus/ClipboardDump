2944813bf321abaf2003c47c8d0330151de8755d
dinar <dinar.mobile@gmail.com>
2022-10-05 23:50:23 +0300

    template

---
+++ ClipboardDump/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace ClipboardDump
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
