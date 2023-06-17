c20d69bc62b4269d30534734edfc8ee91bd6666f
dinar <dinar.mobile@gmail.com>
2023-06-17 11:45:55 +0300

    CSCommon update

--- ClipboardDump/App.xaml.cs
+++ ClipboardDump/App.xaml.cs
@@ -1,16 +1,14 @@
 using System.Windows;
+using CommonWpf.Classes;
 using CommonWpf.Classes.UI;
 
 namespace ClipboardDump
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
 	public partial class App : Application
 	{
 		public App()
 		{
-			ExceptionHandler.Init();
+			AppInitializer.Initialize();
 		}
 	}
 }
